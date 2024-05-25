3287062ccdc302acb05e343372a9582b7618496a
JackeGould <jmgould913@gmail.com>
2024-05-25 14:49:10 -0400

    associations and foreign keys added

--- models/index.js
+++ models/index.js
@@ -4,13 +4,37 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Use foreign keys - link tables together
+// Make sure you set up foreign key relationships that match the column we created in the respective models.
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_to_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_to_products'
+});
 
 module.exports = {
   Product,
